89f2413edd186c2fc038db45a98fe14e565f0c4d
molitormarketing <molitormarketing1@gmail.com>
2026-06-06 13:57:29 -0500

    Update FilmItApp.js

--- components/FilmItApp.js
+++ components/FilmItApp.js
@@ -473,7 +473,7 @@ function LoginScreen({ onLogin }) {
             ))}
             <div style={{ marginTop:20, borderTop:"1px solid rgba(255,255,255,0.2)", paddingTop:16 }}>
               <div style={{ fontSize:11, color:"rgba(255,255,255,0.6)", marginBottom:4 }}>Trusted by agencies managing</div>
-              <div style={{ fontSize:28, fontWeight:700, color:"#fff", lineHeight:1 }}>100+</div>
+              <div style={{ fontSize:28, fontWeight:700, color:"#fff", lineHeight:1 }}>Early Acess</div>
               <div style={{ fontSize:12, color:"rgba(255,255,255,0.6)" }}>creators worldwide</div>
             </div>
           </div>
